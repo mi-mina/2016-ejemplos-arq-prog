31a59d891a135d128f5f45fea6acfad09d43ef9e
Esperanza Moreno Cruz <hola@mi-mina.com>
2017-05-25 21:12:36 +0200

    first commit

---
+++ docs/paint/sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(400, 400);
+  background(0, 220, 220);
+}
+
+function mouseDragged() {
+  noStroke();
+  fill(0);
+  ellipse(mouseX, mouseY, 10, 10);
+  stroke(0);
+  strokeWeight(10);
+  line(mouseX, mouseY, pmouseX, pmouseY);
+  return false;
+}
+
+function keyPressed () {
+  background(0, 220, 220);
+  return false
+}
